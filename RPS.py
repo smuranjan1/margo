cf0d6c96aabf4997bf91e7ce94010d7c2a4d93ed
smuranjan1 <smyan.muranjan@gmail.com>
2024-06-03 18:34:48 -0500

    shhhh

--- RPS.py
+++ RPS.py
@@ -1,7 +1,30 @@
 
 
 def main():
-    pass
+    one = input("User 1 Enter Rock, Paper, or Scissors: ")
+    if one != "Rock" and one != "Paper" and one != "Scissors":
+        print("Error: Enter Rock, Paper, or Scissors Only")
+    two = input("User 2 Enter Rock, Paper, or Scissors: ")
+    if two != "Rock" and two != "Paper" and two != "Scissors":
+        print("Error: Enter Rock, Paper, or Scissors Only")
+    print(RPS(one, two))
+
+def RPS(one, two):
+    if one == "Rock" and two == "Scissors":
+        return "User 1 Wins!"
+    elif one == "Rock" and two == "Paper":
+        return "User 2 Wins!"
+    elif one == "Paper" and two == "Rock":
+        return "User 1 Wins!"
+    elif one == "Paper" and two == "Scissors":  
+        return "User 2 Wins!"
+    elif one == "Scissors" and two == "Paper":
+        return "User 1 Wins!"
+    elif one == "Scissors" and two == "Rock":
+        return "User 2 Wins!"
+    else:
+        return "Draw!"
+    
 
 
 if __name__=="__main__":
