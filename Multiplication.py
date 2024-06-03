f2ee10fd35c2f02cdec24ee2087d2af6e5fa1ad6
molea14 <jlgcbnmc25@gmail.com>
2024-06-03 18:59:13 -0400

    June 3rd

---
+++ Multiplication.py
@@ -0,0 +1,9 @@
+def main ():
+    Hello=0
+    Hi=0
+    Hello=float(input("Give me a number"))
+    Hi=float(input("Give me another number"))
+    print (multiply(Hello,Hi))
+def multiply (one:float, two:float):
+    return (one*two)
+main()
